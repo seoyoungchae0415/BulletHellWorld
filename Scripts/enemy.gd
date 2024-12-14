f188bcf25162546889085eed00867e08ad2800ff
seoyoungchae0415 <seoyoungchae0415@gmail.com>
2024-12-13 22:37:42 -0600

    added enemy movement and projectile pot

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,15 +1,31 @@
 extends Node2D
 
-signal fire_fish
+@onready var bullet_manager: Node2D = $"../BulletManager"
+@onready var lasagna: CharacterBody2D = $"../Lasagna"
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
 
+var rng = RandomNumberGenerator.new()
+
+signal fire_fish
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _process(delta: float) -> void:
-	pass
+	var xpos = rng.randf_range(10, 1000)
+	var ypos = rng.randf_range(10, 600)
+	position = Vector2(xpos, ypos)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("HIT")
+	if body.get_parent() == bullet_manager:
+		on_hit(body)
+	else:
+		print("WHT")
+
+func on_hit(type):
+	if type.name == "laser":
+		print("UHOH")
+		animation_player.play("on_hit_laser")
+	type.queue_free()
